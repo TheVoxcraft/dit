d8739997f1a0366a7a74c70f7877c7d6d122e934
Jonas Silva <jonaslsa@uio.no>
2022-09-08 20:26:09 +0200

    Refactor ditamaster

--- go.mod
+++ go.mod
@@ -3,5 +3,4 @@ module github.com/TheVoxcraft/dit
 go 1.19
 
 require github.com/akamensky/argparse v1.4.0
-
-require github.com/nightlyone/lockfile v1.0.0 // indirect
+require github.com/nightlyone/lockfile v1.0.0
--- pkg/ditmaster/ditmaster.go
+++ pkg/ditmaster/ditmaster.go
@@ -9,9 +9,21 @@ import (
 	"github.com/nightlyone/lockfile"
 )
 
+const (
+	DitPath      = "/.dit/"
+	ManifestPath = "/.dit/manifest"
+	MasterPath   = "/.dit/master"
+	LockFilePath = "/.dit/dit.lock"
+)
+
 type DitMaster struct {
 	Manifest map[string]string
 	Master   map[string]string
+}
+
+var diskStores = DitMaster{
+	Manifest: make(map[string]string),
+	Master:   make(map[string]string),
 }
 
 var Stores = DitMaster{
@@ -19,10 +31,35 @@ var Stores = DitMaster{
 	Master:   make(map[string]string),
 }
 
+func SyncStores() error {
+	// sync the stores to disk
+	err := syncStoreToDisk(diskStores.Manifest, Stores.Manifest, ManifestPath)
+	if err != nil {
+		return err
+	}
+	err = syncStoreToDisk(diskStores.Master, Stores.Master, MasterPath)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func syncStoreToDisk(disk map[string]string, store map[string]string, path string) error {
+	dirty := false
+	for key, value := range store {
+		disk[key] = value
+		dirty = true
+	}
+	if dirty {
+		KVSave(path, disk)
+	}
+	return nil
+}
+
 func CleanDitFolder(path string) error {
-	ditFolder := path + "/.dit"
+	ditFolderPath := filepath.Join(path, DitPath)
 	// delete the folder
-	err := os.RemoveAll(ditFolder)
+	err := os.RemoveAll(ditFolderPath)
 	if err != nil {
 		log.Fatal(err)
 		return err
@@ -31,15 +68,15 @@ func CleanDitFolder(path string) error {
 }
 
 func InitDitFolder(path string) error {
-	ditFolder := path + "/.dit"
+	ditFolderPath := filepath.Join(path, DitPath)
 
 	// create a folder called .dit
-	err := os.Mkdir(ditFolder, 0755)
+	err := os.Mkdir(ditFolderPath, 0755)
 	if err != nil {
 		log.Fatal(err)
 	}
 	// create Lock file
-	lockPath, err := filepath.Abs(ditFolder + "/dit.lock")
+	lockPath, err := filepath.Abs(filepath.Join(path, LockFilePath))
 	if err != nil {
 		return err
 	}
@@ -47,17 +84,18 @@ func InitDitFolder(path string) error {
 	if err != nil {
 		return err
 	}
-	err = lock.TryLock()
-	defer lock.Unlock()
-	if err != nil {
+
+	if err = lock.TryLock(); err != nil { // Try to lock the file
 		return err
 	}
+	defer lock.Unlock() // Unlock the file when we're done
+
 	// create manifest file
-	err = newManifestFile(ditFolder)
+	err = newManifestFile(ditFolderPath)
 	if err != nil {
 		return err
 	}
-	err = newMasterRecord(ditFolder)
+	err = newMasterRecord(ditFolderPath)
 	if err != nil {
 		return err
 	}
@@ -68,7 +106,7 @@ func InitDitFolder(path string) error {
 
 func newManifestFile(path string) error {
 	// create a manifest file
-	_, err := os.Create(path + "/manifest")
+	_, err := os.Create(filepath.Join(path, ManifestPath))
 	if err != nil {
 		return err
 	}
@@ -76,20 +114,20 @@ func newManifestFile(path string) error {
 	Stores.Manifest["repo_path"] = "/my-project/v1.0/"
 	Stores.Manifest["trackers"] = "dit.jonaslsa.com"
 	Stores.Manifest["public_key"] = "(public key)"
-	err = KVSave(path+"/manifest", Stores.Manifest)
+	err = KVSave(filepath.Join(path, ManifestPath), Stores.Manifest)
 	return err
 }
 
 func newMasterRecord(path string) error {
 	// create a master record file
-	_, err := os.Create(path + "/master")
+	_, err := os.Create(filepath.Join(path, MasterPath))
 	if err != nil {
 		return err
 	}
 
 	Stores.Master[".dit/master"] = "1"
 	Stores.Master[".dit/manifest"] = "1"
-	err = KVSave(path+"/master", Stores.Master)
+	err = KVSave(filepath.Join(path, MasterPath), Stores.Master)
 	if err != nil {
 		return err
 	}
